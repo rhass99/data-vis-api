93072df002a80e2dbd0985d358f03c9652a78dc8
Rami H <rhass99@gmail.com>
2018-10-24 18:46:52 -0700

    encrypt password functions working

--- src/core/server.js
+++ src/core/server.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {
+  genHash,
+  compareHash,
+} from './encrypt';
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -9,6 +13,15 @@ app.use(bodyParser.json());
 
 // Authentication routes
 //  - Signup
+app.post('/auth/signup', (req, res, next) => {
+  genHash(req.body.password)
+    .then((password) => {
+      compareHash(req.body.password, password)
+        .then(data => res.status(200).send(data))
+        .catch(err => next(err));
+    }).catch(err => next(err));
+});
+
 //  - Login
 
 // User routes
